bb5b0611cbc4e710f5748cdd8dab32a2ffaf6dc0
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2025-01-18 20:23:19 -0600

    Refactor revalidateDashboardUrls function to remove async keyword for consistency

--- server/actions/actions.config.ts
+++ server/actions/actions.config.ts
@@ -1,10 +1,8 @@
-"use server";
 
 import { revalidatePath } from "next/cache";
 
-export const revalidateDashboardUrls = async () => revalidatePath("/dashboard/urls");
+export const revalidateDashboardUrls = () => revalidatePath("/dashboard/urls");
 
-export const UNAUTHETICATION_ERROR =
-  "You must be logged in to perform this action.";
+export const UNAUTHETICATION_ERROR = "You must be logged in to perform this action.";
 export const URL_NOT_FOUND_ERROR = "URL not found";
 export const TAG_NOT_FOUND_ERROR = "Tag not found";
